d3d0b18aa61600df10b2b267b29df87c4fc9d9c5
Camila <alvescamila87@gmail.com>
2024-02-19 17:31:41 -0300

    create new ids to buttons and fields from html and start code from js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+function createTodoItem() {
+
+}
+
+function cleanFormTodoItem() {
+    document.getElementById('todo_name_field').value = "";
+    document.getElementById('todo_description_field').value = "";
+    document.getElementById('todo_done_field').value = false;
+    document.getElementById('todo_priority_field').value = 0;
+}
