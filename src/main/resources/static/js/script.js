dfdb19b0daa770943027eef78b519fcfaaab2dd2
Camila <alvescamila87@gmail.com>
2024-03-03 22:06:53 -0300

    create function search todo by id

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,6 +5,12 @@ function toogleSpinnerVisibility(visible) {
 
 }
 
+function toggleSections() {
+    const searchByIdSection = document.getElementById('searchByIdSection');
+    searchByIdSection.style.display = searchByIdSection.style.display === 'none' ? 'block' : 'none';
+}
+
+
 function createTodoItem() {
 
     const timeoutMilliseconds = 300;
@@ -13,19 +19,19 @@ function createTodoItem() {
     // Loading toogle during ToDo creation 
     toogleSpinnerVisibility(true);
 
-    var todo_name = document.getElementById('todo_name_field').value;
-    var todo_description = document.getElementById('todo_description_field').value;
-    var todo_done = document.getElementById('todo_done_field').checked;
-    var todo_priority = document.getElementById('todo_priority_field').value;
+    const apiURL = 'http://localhost:8080/todos';
 
-    var todoData = {
+    let todo_name = document.getElementById('todo_name_field').value;
+    let todo_description = document.getElementById('todo_description_field').value;
+    let todo_done = document.getElementById('todo_done_field').checked;
+    let todo_priority = document.getElementById('todo_priority_field').value;
+
+    let todoData = {
         name: todo_name,
         description: todo_description,
         done: todo_done,
         priority: todo_priority
-    };
-
-    const apiURL = 'http://localhost:8080/todos'; 
+    };    
 
     fetch(apiURL, {
         method: 'POST',
@@ -58,23 +64,59 @@ function cleanID() {
 }
 
 function itemTodoResult(result) {
-    var boxResultTodoCreated = document.querySelector('.box-result-todo-created');
+    let boxResultTodoDetails = document.querySelector('.box-result-todo-details');
 
-    boxResultTodoCreated.innerHTML = '';
+    boxResultTodoDetails.innerHTML = '';
 
     if(result && result.id && result.name && result.description && result.done !== undefined && result.priority !== undefined) {
 
-        boxResultTodoCreated.innerHTML += '<h4> ToDo item created: </h4>' + '<p>ID: ' + result.id  + '</p>';
-        boxResultTodoCreated.innerHTML += '<p>Name: ' + result.name + '</p>';
-        boxResultTodoCreated.innerHTML += '<p>Description: ' + result.description + '</p>';
-        boxResultTodoCreated.innerHTML += '<p>Done: ' + result.done + '</p>';
-        boxResultTodoCreated.innerHTML += '<p>Priority: ' + result.priority + '</p>';
+        boxResultTodoDetails.innerHTML += '<h4> ToDo item created: </h4>' + '<p>ID: ' + result.id  + '</p>';
+        boxResultTodoDetails.innerHTML += '<p>Name: ' + result.name + '</p>';
+        boxResultTodoDetails.innerHTML += '<p>Description: ' + result.description + '</p>';
+        boxResultTodoDetails.innerHTML += '<p>Done: ' + result.done + '</p>';
+        boxResultTodoDetails.innerHTML += '<p>Priority: ' + result.priority + '</p>';
+        boxResultTodoDetails.innerHTML += '<button type="button" class="btn btn-secondary btn-sm" onclick="toggleSections()">>Back 2</button>';     
 
 
     } else {
 
         console.error('Error to return data from ToDo Item: ', result);
-        boxResultTodoCreated.innerHTML = '<p>Error when processing data from item ToDo. </p>';
+        boxResultTodoDetails.innerHTML = '<p>Error when processing data from item ToDo. </p>';
     }
     
 }
+
+function searchByID() {
+
+    const timeoutMilliseconds = 300;
+    const timeoutPromise = new Promise((resolve) => setTimeout(resolve, timeoutMilliseconds));
+
+    // Loading tootle during search by id
+    toogleSpinnerVisibility(true);
+
+    const apiURLById = `http://localhost:8080/todos/${todoId}`;
+
+    let todo_id = document.getElementById('todo_search_field').value;
+
+    fetch(apiURLById, {
+        method: 'GET',
+    })
+        .then(response => {
+            if(response.ok) {
+                return response.json();                
+            } else {
+                throw new Error('Failed to fetch todo');
+            }
+        })
+        .then(data => {
+            itemTodoResult(data);
+        })
+        .catch(error => console.error('Error to search ToDo ID: ', error))
+        .finally(() => {
+            document.querySelector('.box-container-todo').style.display = 'none';
+            document.querySelector('.box-container-todo-list').style.display = 'none';
+            document.querySelector('.box-result-all-todos').style.display = 'none';
+            toogleSpinnerVisibility(false);
+        });
+}
+
